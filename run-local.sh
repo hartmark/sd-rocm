6dd180aca2373e606ffdfd5996af5c22908ce317
Markus Hartung <mail@hartmark.se>
2025-01-13 03:47:23 +0100

    Added cpu-only mode

--- run-local.sh
+++ run-local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ###
 # Running on arch you need to install:
-# rocminfo 
+# rocminfo - unless you're using cpuonly
 # pyenv
 ###
 
 PYTHON_VERSION="3.12"
-ROCM_VERSION="release"
+ROCM_VERSION="cpuonly"
 export DOCKER_INSTANCE="local-comfyui"
 ROOT_DIR="${PWD}/data/home-local"
 
@@ -14,7 +14,7 @@ if [ ! -d "${ROOT_DIR}" ]; then
   mkdir "${ROOT_DIR}"
 fi
 
-COMFYUI_PORT=8080
+COMFYUI_PORT=31490
 
 source conf/functions.sh
 has_rocm
